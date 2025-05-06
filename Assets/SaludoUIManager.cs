0909b2294164e30db7616e896785551b5dd76a7c
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2025-05-06 12:14:46 -0300

    Ejercicio Saludo terminado

---
+++ Assets/SaludoUIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SaludoUIManager : MonoBehaviour
+{
+    public TextMeshProUGUI txtSaludo;
+    public TMP_InputField inputNombre;
+    string nombreIngresado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtSaludo.text = "";
+    }
+
+    public void Saludar()
+    {
+        txtSaludo.text = "Hola " + inputNombre.text;
+    }
+}
